02bbec362af8a00fe1caf83552ae2745a5a5bc1f
unknown <dhurtado.freddy@gmail.com>
2020-10-15 01:51:42 -0300

    Creando modelos

--- locallibreria/dotgames/admin.py
+++ locallibreria/dotgames/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Genero, Juego
+
+admin.site.register(Juego)
+admin.site.register(Genero)
+
--- locallibreria/dotgames/models.py
+++ locallibreria/dotgames/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse 
+import uuid 
 
-# Create your models here.
+class Genero(models.Model):
+
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Juego(models.Model):
+
+    titulo = models.CharField(max_length=100)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID pal juego gei')
+    descripcion = models.TextField(max_length=1000, help_text='Descripción del juego')
+    autor = models.CharField(max_length=100)
+    genero = models.ManyToManyField(Genero)
+
+    def __str__(self):
+        return self.titulo
+
+    
+    
